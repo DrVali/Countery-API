20d046406fe00532c56b339d3b4f0320117228ac
Ali Ahmadi <ahmadi.world@gmail.com>
2024-11-12 01:01:45 +0330

    Add files via upload

--- js/mainjs.js
+++ js/mainjs.js
@@ -4,8 +4,6 @@ window.onload =function (){
     //create div class="container-fluid"
     const containerEl =document.createElement('div');
     containerEl.classList.add("container-fluid");
-    
-
     const mainHeader =document.createElement("div");
     mainHeader.classList.add("header");
     containerEl.append(mainHeader);
@@ -13,7 +11,7 @@ window.onload =function (){
 //create div class="row"
     const rowdivRow =document.createElement("div");
     rowdivRow.classList.add("row");
-    rowdivRow.classList.add("header");
+    
     document.body.append(containerEl);
     mainHeader.append(rowdivRow);
 
@@ -25,7 +23,7 @@ window.onload =function (){
     cols8.classList.add("col-md-8");
     cols8.classList.add("col-sm-8");
     
-    //create div class="col-4"
+    //create div class="col-4 "
     const col4 =document.createElement("div");
     col4.classList.add("col-lg-4");
     col4.classList.add("col-md-4");
@@ -44,7 +42,7 @@ window.onload =function (){
   
     
 
-
+           //change moon or sun
     if(document.body.classList.contains("dark")){
         document.getElementById("darkI").classList.add("fa-sun-o");
     }
@@ -53,21 +51,14 @@ window.onload =function (){
 
     
     function darkmode(){
-
-
         document.getElementById("darkI").classList.toggle("fa-sun-o");
         document.getElementById("darkI").classList.toggle("fa-moon-o");
         document.body.classList.toggle("dark");
-     
-
-
-    } 
-
-
+         } 
       btnDarkmode.addEventListener("click",darkmode);
 
-    
 
+      //create header left
    const textHeader=document.createElement("p");
    textHeader.innerHTML="where in the world?";
    textHeader.classList.add("lHeader");
@@ -76,15 +67,136 @@ window.onload =function (){
     
 
 
+ //create don header for search and ....
+
+   const downHeader= document.createElement("div");
+   downHeader.classList.add("row");
+   containerEl.append(downHeader);
+
+//create div class="col-8"
+
+const col7 =document.createElement("div");
+col7.classList.add("col-lg-7");
+col7.classList.add("col-md-7");
+col7.classList.add("col-sm-7");
 
 
+//create div class="col-4 "
+const cols5 =document.createElement("div");
+cols5.classList.add("col-lg-5");
+cols5.classList.add("col-md-5");
+cols5.classList.add("col-sm-5");
+cols5.classList.add("header-r");
+
+downHeader.append(col7);
+downHeader.append(cols5);
 
 
+//create search box
+  const searchEl=document.createElement("div");
+   searchEl.classList.add("search");
+   const iconEl =document.createElement("div");
+   iconEl.classList.add("icon");
+   const inputEl =document.createElement("div");
+   inputEl.classList.add("input");
+   const inputTextSearch = document.createElement("input");
+   inputTextSearch.type="text";
+   inputTextSearch.placeholder="Search";
+   inputTextSearch.id="mySearch";
+   const spanEl=document.createElement("span");
+   spanEl.classList.add("clear");
+   inputEl.append(inputTextSearch);
+   inputEl.append(spanEl);
+   searchEl.append(iconEl);
+   searchEl.append(inputEl);
+   col7.append(searchEl);
+   
 
+   //create drop down
+   const dropDownEl=document.createElement("div");
+   dropDownEl.classList.add("dropdown");  
+    
+    const selectbox=document.createElement("select");
+    selectbox.name="region";
+    selectbox.id="region";
+    selectbox.classList.add("regionClss");
+
+    dropDownEl.append(selectbox);
+
+    const option1=document.createElement("option");
+    option1.value="select a Region";
+    option1.innerHTML="";
+    option1.id="select a Region";
+    option1.style.display="none";
+    selectbox.append(option1);
+
+    const option2=document.createElement("option");
+    option2.value="Africa";
+    option2.innerHTML="Africa";
+    option2.id="Africa";
+    selectbox.append(option2);
 
    
+    const option3=document.createElement("option");
+    option3.value="America";
+    option3.innerHTML="America";
+    option3.id="America";
+    selectbox.append(option3);
+
     
+    const option4=document.createElement("option");
+    option4.value="Asia";
+    option4.innerHTML="Asia";
+    option4.id="Europe";
+    selectbox.append(option4);
+
+
     
+    const option5=document.createElement("option");
+    option5.value="Europe";
+    option5.innerHTML="Europe";
+    option5.id="Europe";
+    selectbox.append(option5);
     
+
+    const option6=document.createElement("option");
+    option6.value="Oceania";
+    option6.innerHTML="Oceania";
+    option6.id="Oceania";
+    selectbox.append(option6);
+
+  cols5.append(dropDownEl);
+
+   function closeSearch(){
+    document.getElementById('mySearch').value='';
+   }
+   searchEl.addEventListener("click", closeSearch);
+
+
+
+//serch 
+  const icon= document.querySelector('.icon');
+  const search= document.querySelector('.search');
+    icon.addEventListener("click", function(){
+         search.classList.toggle('active');
+    }
+    );
+    
+
+
+    let request;
+    if(window.XMLHttpRequest) { request= new XMLHttpRequest(); }
+    else { request = new ActiveXObject("Microsoft.XMLHTTP"); }
+    request.open('GET', 'data.json') ;
+    
+    request.send();
+
+ request.onreadystatechange=function (){
+    if (request.readyState == 4 && request.status == 200) {
+        console.log("ghghj");
+        let result= request.responseText;
+    }
+}
+
     }
     
